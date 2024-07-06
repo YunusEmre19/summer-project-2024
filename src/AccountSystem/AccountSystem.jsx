f5dbd80f235768d08d81ff4592bf2a147fee90f1
Yunus Emre İşan <59234031+YunusEmre19@users.noreply.github.com>
2024-07-06 17:58:23 +0300

    Account System updates

--- src/AccountSystem/AccountSystem.jsx
+++ src/AccountSystem/AccountSystem.jsx
@@ -1,6 +1,23 @@
 import {useState} from "react"
 import styles from "./AccountSystem.module.css";
 
+function CreateAccount({handleNewAccount:handleNewAccount, result:result}){
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [userUrl, setUserUrl] = useState("");
+
+  return(
+    <div className="box">
+      <h1>Create Account</h1>
+      <input type="text" placeholder="name" value={name} onChange={(e) => {setName(e.target.value)}}/>
+      <input type="text" placeholder="password" value={password} onChange={(e) => {setPassword(e.target.value)}}/>
+      <input type="text" placeholder="url" value={userUrl} onChange={(e) => {setUserUrl(e.target.value)}}/>
+      <button onClick={() => handleNewAccount(name, password, userUrl)}>Create Account</button>
+      {result}
+    </div>
+  )
+}
+
 function Login({handleLogin:handleLogin, result:result}){
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
@@ -17,46 +34,53 @@ function Login({handleLogin:handleLogin, result:result}){
   )
 }
 
-function CreateAccount({handleNewAccount:handleNewAccount}){
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-  const [userUrl, setUserUrl] = useState("");
-  return(
-    <div className="box">
-      <h1>Create Account</h1>
-      <input type="text" placeholder="name" value={name} onChange={(e) => {setName(e.target.value)}}/>
-      <input type="text" placeholder="password" value={password} onChange={(e) => {setPassword(e.target.value)}}/>
-      <input type="text" placeholder="url" value={userUrl} onChange={(e) => {setUserUrl(e.target.value)}}/>
-      <button onClick={() => handleNewAccount(name, password, userUrl)}>Create Account</button>
-    </div>
-  )
-
-}
-
 function AccountSystem(){
   const [accounts, setAccounts] = useState([{name:"walter", password:"finger", url:"https://media1.tenor.com/m/nicIRWJkKIwAAAAC/walter-white-walter.gif"}]);
-  const [result, setResult] = useState(null);
+  const [loginResult, setLoginResult] = useState(null);
+  const [newAccResult, setNewAccResult] = useState(null);
+
   function handleNewAccount(name, password, userUrl){
-    // ToDo: Prevent creation of duplicate accounts
-    //       Add secure password verification
+
+    if(name.length < 4){
+      setNewAccResult(<span>Username cannot be shorter than 4 characters</span>);
+      return;
+    }
+
+    for(let i = 0; i<accounts.length; i++){
+      if(accounts[i].name === name){
+        setNewAccResult(<p>Username already taken</p>);
+        return;
+      }
+    }
+
+    if(password.length<8){
+      setNewAccResult(<p>Password must be at least 8 characters</p>);
+      return;
+    }
+
+    setNewAccResult(<p>Account has been created</p>);
     setAccounts(a=>[...a, {name:name, password:password, url:userUrl}]);
   }
 
   function handleLogin(name, password){
-    // ToDo: Check for password
-    //       Display warning when the name/password is wrong
     for (let i = 0; i < accounts.length; i++){
       if (accounts[i].name === name){
-        setResult(<img className={styles.image} src={accounts[i].url}></img>);
+        if(accounts[i].password === password){
+          setLoginResult(<img className={styles.image} src={accounts[i].url}></img>);
+        }
+        else {
+          setLoginResult(<p>Wrong Password</p>);
+        }
         return;
       }
-    }
-    
+    } 
+    setLoginResult(<p>Account not found</p>);
   }
+
   return(
     <>
-      <CreateAccount handleNewAccount={handleNewAccount}/>
-      <Login handleLogin={handleLogin} result={result}/>
+      <CreateAccount handleNewAccount={handleNewAccount} result={newAccResult}/>
+      <Login handleLogin={handleLogin} result={loginResult}/>
     </>
   )
 }
